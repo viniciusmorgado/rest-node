897902c4f4cc675a24ee3f08cff4386fcb504288
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2021-04-10 01:43:12 -0300

    Implements startup file app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+// const { Mongoose } = require("mongoose");
+var app = express();
+
+// Route
+app.get("/", function(req, res){
+    res.send("hello world");
+});
+
+// MongoDb Connection
+mongoose.connect("mongodb://localhost/test", {userNewUrlParser:true});
+
+mongoose.connection.once("open", function() {
+    console.log("Database connected successfully");
+}).on("Error", function(err){
+    console.log("Error", err);
+});
+
+//Server
+app.listen(8000, function(){
+    console.log("Server is up");
+});
